94c3f97fbbb68ef5b85c5e510cf00dc0b0b4b7d0
Kingupdollar <42480845+Kingupdollar@users.noreply.github.com>
2021-07-19 01:33:44 +0800

    Create firebase-connector.js

---
+++ script/firebase-connector.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAEMxX5UXEd3fzojfiyWrhPXu5vb7qC2Ps",
+    authDomain: "beyond-perfect-learning-tool.firebaseapp.com",
+    projectId: "beyond-perfect-learning-tool",
+    storageBucket: "beyond-perfect-learning-tool.appspot.com",
+    messagingSenderId: "1001127952321",
+    appId: "1:1001127952321:web:9ca643a8488d1158bcf940",
+    measurementId: "G-KSGYMQN4K4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
